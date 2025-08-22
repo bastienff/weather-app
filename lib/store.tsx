0e350fe7b1b3d2ec229374cf1d074190ccc767ff
Sebastián Fuentes <bastienff@gmail.com>
2025-08-22 15:32:24 -0400

    feat: Add zustand store

--- lib/store.tsx
+++ lib/store.tsx
@@ -3,10 +3,13 @@ import { create } from 'zustand'
 
 export type StoreStateType = {
   selectedCity: MenuCityType;
+  weatherData: any,
   setSelectedCity: (city: MenuCityType) => void;
 }
 
 export const useStore = create<StoreStateType>((set) => ({
   selectedCity: MENU_CITIES[0],
+  weatherData: {},
   setSelectedCity: (city: MenuCityType) => set({ selectedCity: city }),
+  setWeatherData: (data: any) => set({ weatherData: data }),
 }))
