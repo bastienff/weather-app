18b1b3c90d202ad7c99bdaf0fe98913825bc085d
Sebastián Fuentes <bastienff@gmail.com>
2025-08-22 10:40:19 -0400

    feat: Add header

---
+++ components/header.tsx
@@ -0,0 +1,24 @@
+import { Search } from '@/components/search';
+import { AppBar, Toolbar, Typography, Dialog, DialogTitle, DialogContent, DialogActions, Button } from '@mui/material';
+import React, { useState } from 'react'
+
+export const Header = () => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const handleDialogClose = () => {
+    setIsDialogOpen(false);
+  };
+
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" flexGrow={1} noWrap >
+            Simple Weather
+          </Typography>
+          <Search />
+        </Toolbar>
+      </AppBar>
+    </>
+  )
+}
